3e10aae65a7885e135c06e6ef89e69a47940e6c5
geeeger <lph1988520@sina.com>
2019-02-13 10:56:34 +0800

    build: config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,12 @@
 const path = require("path");
-const webpack = require("webpack");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 const config = {
     entry: {
-        // "normal": "./src/QETag.ts",
-        // "base": "./src/QETagBase.ts",
-        "file": "./src/QZFile.ts",
-        // "worker": "./src/QETagWorker.ts",
+        "Normal": "./src/QETag.ts",
+        "Base": "./src/QETagBase.ts",
+        "File": "./src/QZFile.ts",
+        "Worker": "./src/QETagWorker.ts",
     },
     resolve: {
         extensions: [".ts", ".js"],
@@ -15,7 +14,7 @@ const config = {
     output: {
         path: path.resolve(__dirname, "lib"),
         libraryTarget: "umd",
-        library: ["QETag", "[name]"],
+        library: ["qetag", "[name]"],
         libraryExport: "default"
     },
     module: {
@@ -40,11 +39,11 @@ const config = {
 
 module.exports = (_, argv) => {
     if (argv.mode === "development") {
-        config.output.filename = "qetag-[name].js";
+        config.output.filename = "qetag.[name].js";
     }
 
     if (argv.mode === "production") {
-        config.output.filename = "qetag-[name].min.js";
+        config.output.filename = "qetag.[name].min.js";
         config.optimization = {};
         config.optimization.minimizer = [
             new UglifyJsPlugin()
---
+++ webpack.worker.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
+module.exports = function() {
+    return {
+        entry: './src/WorkerScript.ts',
+        devtool: 'source-map',
+        mode: "production",
+        resolve: {
+            extensions: ['.ts', '.js'],
+        },
+        output: {
+            filename: 'index.js',
+            path: path.resolve(__dirname, 'worker'),
+            libraryTarget: 'var',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: ["ts-loader"],
+                    enforce: "pre",
+                },
+                {
+                    test: /\.ts$/,
+                    enforce: "pre",
+                    loader: "tslint-loader",
+                    exclude: /(node_modules)/,
+                    options: {
+                        /* Loader options go here */
+                    },
+                },
+            ],
+        },
+        optimization: {
+            minimizer: [
+                new UglifyJsPlugin()
+            ]
+        }
+    };
+};
