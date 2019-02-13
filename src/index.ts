133f16864918c4233eb997ea11880d0a721e3b37
geeeger <lph1988520@sina.com>
2019-02-13 15:23:57 +0800

    feat: export WorkerProvider add github page

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import ws from "worker-provider";
+import * as utilsMethod from "./utils";
+
 export { default as Base } from "./base";
 
 export { default as Block } from "./block";
@@ -7,3 +10,7 @@ export { default as File } from "./file";
 export { default as Normal } from "./normal";
 
 export { default as Worker } from "./worker";
+
+export const WorkerProvider = ws;
+
+export const utils = utilsMethod;
